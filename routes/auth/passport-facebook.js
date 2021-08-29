8078cda5ff7dfbe927aa5e004b9173404135ff12
Wai Phyo Naing <51906662+waiphyo285@users.noreply.github.com>
2021-08-29 09:19:24 +0630

    test book auth

--- routes/auth/passport-facebook.js
+++ routes/auth/passport-facebook.js
@@ -8,8 +8,8 @@ module.exports  = new FacebookStrategy({
     clientSecret: FACEBOOK_APP_SECRET,
     callbackURL: "https://itemplate-marketplace.herokuapp.com/auth/facebook/callback",
     // profileFields: ['id', 'emails', 'username', 'displayName', 'name', 'gender', 'profileUrl', 'photos'],
-    profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified'],
-    // profileFields: ['id', 'displayName', 'photos', 'email', 'username'],
+    // profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified'],
+    profileFields: ['id', 'displayName', 'photos', 'email', 'gender'],
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {
