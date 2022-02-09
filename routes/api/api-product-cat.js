2bf1d42cb65b600ff43375d25e32e11aeb736796
waiphyo285 <waiphyoenaing.joy007@gmail.com>
2022-02-09 12:57:40 +0630

    update images

--- routes/api/api-product-cat.js
+++ routes/api/api-product-cat.js
@@ -1,17 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var axiosHandler = require("../../library/axios-handler");
 
-router.get('/api/getProductCategory', function (req, res, next) {
-    axiosHandler.module.get('/api/product_category', {
-      params: req.query
+router.get("/api/getProductCategory", function (req, res, next) {
+  axiosHandler.module
+    .get("/api/product_category", {
+      params: req.query,
     })
     .then(function (response) {
-        res.send(response.data)
+      console.log("HELLO WORLD");
+      res.send(response.data);
     })
     .catch(function (error) {
-        res.send(error)
-    })
+      res.send(error);
+    });
 });
 
 module.exports = router;
